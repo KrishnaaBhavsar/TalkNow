361732c522a48ad4c081de7cac383be81f113953
KrishnaaBhavsar <krishnabhavsar7425@gmail.com>
2025-07-16 09:49:10 +0530

    completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,7 @@ import messageRoutes from './routes/message.js'
 import {connectDB} from './lib/db.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import path from 'path';
 import { app,server } from './lib/socket.js';
 
 
@@ -14,6 +15,7 @@ import { app,server } from './lib/socket.js';
 
 
 const PORT= process.env.PORT ;
+const __dirname=path.resolve();
 
 app.use(express.json({limit:'50mb'}));
 app.use(cookieParser());
@@ -27,6 +29,16 @@ app.use(cors({
 
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
+
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend" , "dist" , "index.html"));
+    })
+}
 
 
 
